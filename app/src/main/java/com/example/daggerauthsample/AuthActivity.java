fadf27e503c63a320a1c7156f3e9c52fa637ebf6
Maksymilian Wojcik <maksymilian.wojcik@acasus.com>
2019-09-21 19:11:41 +0200

    Injecting logo into auth activity

--- app/src/main/java/com/example/daggerauthsample/AuthActivity.java
+++ app/src/main/java/com/example/daggerauthsample/AuthActivity.java
@@ -2,15 +2,39 @@ package com.example.daggerauthsample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.RequestManager;
+
+import javax.inject.Inject;
 
 import dagger.android.support.DaggerAppCompatActivity;
 
 public class AuthActivity extends DaggerAppCompatActivity { // DaggerAppCompatActivity cause we use android dagger classes
 
+
+    @Inject
+    Drawable logo;
+
+    @Inject
+    RequestManager requestManager;
+
+    ImageView logoImageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+        logoImageView = findViewById(R.id.login_logo);
+
+        setLogo();
+    }
+
+    private void setLogo() {
+        requestManager
+                .load(logo)
+                .into(logoImageView);
     }
 }
