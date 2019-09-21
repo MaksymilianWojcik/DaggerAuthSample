7ec6122f56dfd6cf6a565c47594a01b1a348b504
Maksymilian Wojcik <maksymilian.wojcik@acasus.com>
2019-09-21 22:19:11 +0200

    Added logout menu button

--- app/src/main/java/com/example/daggerauthsample/ui/main/MainActivity.java
+++ app/src/main/java/com/example/daggerauthsample/ui/main/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.daggerauthsample.ui.main;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.daggerauthsample.BaseActivity;
 import com.example.daggerauthsample.R;
@@ -13,5 +17,25 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.logout: {
+                sessionManager.logOut();
+                return true;
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
